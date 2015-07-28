bce0288707bfff14af79893a687e188e721c4295
Finjon Kiang <kiange@gmail.com>
2015-07-29 00:13:09 +0800

    update data

--- 01_extract_files.php
+++ 01_extract_files.php
@@ -6,7 +6,7 @@ if (!file_exists($tmpPath)) {
 }
 
 $za = new ZipArchive();
-$za->open($tmpPath . '/20150526143758836.zip');
+$za->open($tmpPath . '/20150729000806997.zip');
 
 for ($i = 0; $i < $za->numFiles; $i++) {
     $info = $za->statIndex($i);
