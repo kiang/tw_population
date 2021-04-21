5186dab24d07e983304a0a94b56c0ff42fb7961d
kiang <kiange@gmail.com>
2021-04-21 16:37:47 +0800

    get raw data from https://data.gov.tw/dataset/131138

--- 01_raw.php
+++ 01_raw.php
@@ -2,6 +2,29 @@
 //http://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=F7E9A1B3-3985-434A-9195-4EB940400D59
 // old: https://data.gov.tw/dataset/32973
 
+$json = json_decode(file_get_contents('https://data.gov.tw/api/v2/rest/dataset/131138'), true);
+foreach($json['result']['distribution'] AS $item) {
+  if($item['resourceFormat'] === 'CSV') {
+    $item['resourceDescription'] = trim($item['resourceDescription']);
+    $y = intval(substr($item['resourceDescription'], 0, 3)) + 1911;
+    if($y > 2000) {
+      $pos = strpos($item['resourceDescription'], '月');
+      $m = substr($item['resourceDescription'], 3, 2);
+      $targetPath = __DIR__ . "/updates/{$y}/{$m}";
+      if (!file_exists($targetPath)) {
+        mkdir($targetPath, 0777, true);
+      }
+      $targetFile = $targetPath . '/data.csv';
+      if(!file_exists($targetFile)) {
+        $content = shell_exec("curl '{$item['resourceDownloadUrl']}' -H 'Host: data.moi.gov.tw' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1'");
+        if(!empty($content)) {
+          file_put_contents($targetFile, $content);
+        }
+      }
+    }
+  }
+}
+
 $json = json_decode(file_get_contents('https://data.gov.tw/api/v2/rest/dataset/77132'), true);
 foreach($json['result']['distribution'] AS $item) {
   if($item['resourceFormat'] === 'CSV') {
