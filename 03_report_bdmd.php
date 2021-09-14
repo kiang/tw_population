a075a2fa2a51c46cc8e98d216677595fc97e2680
kiang <kiange@gmail.com>
2021-09-14 20:58:47 +0800

    birth report

--- 03_report_bdmd.php
+++ 03_report_bdmd.php
@@ -1,6 +1,6 @@
 <?php
 
-$pool = [];
+$death = $birth = [];
 foreach(glob(__DIR__ . '/bdmd/*/*/*.csv') AS $csvFile) {
     $fh = fopen($csvFile, 'r');
     $head = fgetcsv($fh, 2048);
@@ -12,19 +12,35 @@ foreach(glob(__DIR__ . '/bdmd/*/*/*.csv') AS $csvFile) {
         $data = array_combine($head, $line);
         $y = substr($data['統計年月'], 0, 3) + 1911;
         $m = substr($data['統計年月'], 3);
-        if(!isset($pool[$y])) {
-            $pool[$y] = [];
+        if(!isset($death[$y])) {
+            $death[$y] = [];
+            $birth[$y] = [];
         }
-        if(!isset($pool[$y][$m])) {
-            $pool[$y][$m] = 0;
+        if(!isset($death[$y][$m])) {
+            $death[$y][$m] = 0;
+            $birth[$y][$m] = 0;
         }
-        $pool[$y][$m] += $data['死亡數'];
+        $death[$y][$m] += $data['死亡數'];
+        $birth[$y][$m] += $data['出生數'];
     }
 }
-$keys = array_keys($pool[2015]);
+$keys = array_keys($death[2015]);
 $fh = fopen(__DIR__ . '/reports/death.csv', 'w');
 fputcsv($fh, array_merge(['year'], $keys));
-foreach($pool AS $y => $l1) {
+foreach($death AS $y => $l1) {
+    $line = [$y];
+    foreach($keys AS $key) {
+        if(isset($l1[$key])) {
+            $line[] = $l1[$key];
+        } else {
+            $line[] = '';
+        }
+    }
+    fputcsv($fh, $line);
+}
+$fh = fopen(__DIR__ . '/reports/birth.csv', 'w');
+fputcsv($fh, array_merge(['year'], $keys));
+foreach($birth AS $y => $l1) {
     $line = [$y];
     foreach($keys AS $key) {
         if(isset($l1[$key])) {
