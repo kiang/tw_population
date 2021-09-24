3554e2cd1f8da777ea9203a0ec9a263140e615c4
kiang <kiange@gmail.com>
2021-09-24 10:19:48 +0800

    change time begin for 2019 report

--- 03_report_2019.php
+++ 03_report_2019.php
@@ -1,6 +1,6 @@
 <?php
 
-$old = __DIR__ . '/population/2019/01/data.csv';
+$old = __DIR__ . '/population/2018/12/data.csv';
 $new = __DIR__ . '/population/2021/08/data.csv';
 
 $fh = fopen($old, 'r');
@@ -23,12 +23,14 @@ $pairs = [
     '三民二' => '三民區',
     '鳳山一' => '鳳山區',
     '鳳山二' => '鳳山區',
+    '安南區顯宮里' => '安南區媽祖宮里',
+    '仁愛鄉合作村' => '仁愛鄉德鹿谷村',
 ];
 while($line = fgetcsv($fh, 8000)) {
     $key = $line[2] . $line[3];
     $key = strtr($key, $pairs);
     $pool[$key] = [
-        '2019' => $line[5],
+        '2018' => $line[5],
         '2021' => 0,
         'diff' => 0,
         'rate' => 0.0,
@@ -44,8 +46,8 @@ while($line = fgetcsv($fh, 8000)) {
     $key = strtr($key, $pairs);
     if(isset($pool[$key])) {
         $pool[$key]['2021'] = $line[5];
-        $pool[$key]['diff'] = $pool[$key]['2021'] - $pool[$key]['2019'];
-        $pool[$key]['rate'] = round($pool[$key]['diff'] / $pool[$key]['2019'], 3);
+        $pool[$key]['diff'] = $pool[$key]['2021'] - $pool[$key]['2018'];
+        $pool[$key]['rate'] = round($pool[$key]['diff'] / $pool[$key]['2018'], 3);
     }
 }
 
@@ -59,7 +61,7 @@ function cmp($a, $b) {
 uasort($pool, 'cmp');
 
 $oFh = fopen(__DIR__ . '/reports/2019/population.csv', 'w');
-fputcsv($oFh, ['cunli', '2019.01', '2021.08', 'diff', 'rate']);
+fputcsv($oFh, ['cunli', '2018.12', '2021.08', 'diff', 'rate']);
 foreach($pool AS $k => $v) {
-    fputcsv($oFh, [$k, $v['2019'], $v['2021'], $v['diff'], $v['rate']]);
+    fputcsv($oFh, [$k, $v['2018'], $v['2021'], $v['diff'], $v['rate']]);
 }
